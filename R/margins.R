e4086e795823521714147334b565fc36b0fabe06
Thomas J. Leeper <thosjleeper@gmail.com>
2014-10-14 16:27:18 +0200

    work out some complex covariates

--- R/margins.R
+++ R/margins.R
@@ -3,103 +3,157 @@ function(x, atmeans = FALSE, ...) {
     UseMethod("margins")
 }
 
+print.margins <-
+function(x, intercept = FALSE, ...){
+    if(intercept)
+        print.data.frame(x)
+    else
+        print.data.frame(x[!x[["Factor"]] == "(Intercept)",])
+}
+
 margins.lm <- 
-function(x, atmeans = FALSE, ...){
+function(x, 
+         at = NULL, 
+         atmeans = FALSE,
+         ...){
     b.est <- coef(x)
-    mm <- model.matrix(x)
     if(atmeans) {
-        out <- data.frame(Effects = b.est, row.names = names(b.est))
+        tmp <- as.list(colMeans(x$model))
+        if(!is.null(at)){
+            if(any(! names(at) %in% names(x$model)))
+                stop("Unrecognized variable name in 'at'")
+            tmp <- expand.grid(append(tmp, list(a = 1:2)))
+        }
+        x$model <- as.data.frame(tmp)
     } else {
-        out <- data.frame(Effects = b.est, row.names = names(b.est))
+        if(!is.null(at)){
+            if(any(! names(at) %in% names(x$model)))
+                stop("Unrecognized variable name in 'at'")
+            x$model[,names(at)] <- at
+        }
     }
-    s <- xb
-    dr <- (out$Effects/b.est) * (diag(1, ncol(mm), ncol(mm)) + c(s) * (b.est %*% t(as.matrix(colMeans(mm)))))
+    mm <- model.matrix(x) # automatically generates interactions, factors, and I() variables
+    ord <- attributes(terms(x))$order
+    f <- attributes(terms(x))$factors[-1,]
+    # drop non-unique I() terms
+    if(any(grepl("I\\(.+\\)", rownames(f)))) {
+        tmpnames <- gsub("[(I\\()(\\))]", "", rownames(f))
+        tmpnames <- gsub("^[:digit:]+[\\^\\+\\-\\*\\/]", "", tmpnames)
+        tmpnames <- gsub("[^\\+-\\*/][[:digit:]+]$", "", tmpnames)
+        f <- f[unique(tmpnames),]
+    }
+    
+    # first-order effects
+    e <- rowSums(f[, ord == 1] * coef(x)[-1][ord == 1])
+    # VARAINCE CALCULATION (ONLY WORKS IF NO INTERACTION, ETC. TERMS)
+    s <- t(as.matrix(colMeans(mm))) %*% b.est
+    dr <- (e/b.est) * (diag(1, ncol(mm), ncol(mm)) + c(s) * (b.est %*% t(as.matrix(colMeans(mm)))))
     variance <- dr %*% vcov(x) %*% t(dr)
-    out[["Std. Error"]] <- sqrt(diag(variance))
-    out[["t.statistic"]] <- out[["Effects"]]/out[["Std. Error"]]
-    out[["Pr(>|z|)"]] <- 2 * (1 - pt(abs(out[["t.statistic"]]), nrow(mm) - ncol(mm)))
+    
+    # Interaction terms
+    if(any(ord > 1)) {
+        i <- f[, ord > 1, drop = FALSE] * coef(x)[-1][ord > 1]
+        if(nrow(i) > 0){
+            for(j in 1:nrow(i)){
+                g <- grep(rownames(i)[j], colnames(i))
+                if(length(g)){
+                    itmp <- i[,g, drop = FALSE]
+                    sapply(colnames(itmp), function(k){
+                        s <- setdiff(strsplit(k, ":")[[1]], rownames(i)[j])
+                        # MAY NEED SOMETHING HERE TO SKIP I() TERMS
+                        v <- x$model[, s, drop = FALSE]
+                        v <- if(ncol(v)>1) do.call(`*`, v) else v[,1]
+                        e[rownames(i)[j]] <<- e[rownames(i)[j]] + mean(v * b.est[k])
+                    })
+                }
+                # NEED TO DO UPDATED VARIANCE CALCULATIONS
+                # reference: https://files.nyu.edu/mrg217/public/interaction.html
+                # THIS WILL BE TRICKY WITH I() TERMS
+            }
+        }
+        variance <- matrix(NA, nrow=f, ncol=f)
+    } 
+    
+    # format output
+    out <- data.frame(Factor = names(e), 
+                      Effect = e, 
+                      row.names = seq_along(e))
+    #out[["Std. Error"]] <- sqrt(diag(variance))
+    #out[["t.statistic"]] <- out[["Effect"]]/out[["Std. Error"]]
+    #out[["Pr(>|z|)"]] <- 2 * (1 - pt(abs(out[["t.statistic"]]), nrow(mm) - ncol(mm)))
     return(structure(out, class = c("margins", "data.frame")))
 }
 
 margins.glm <- 
-function(x, atmeans = FALSE, at = NULL, ...) {
+function(x, 
+         at = NULL, 
+         atmeans = FALSE, 
+         ...) {
+    # link function
+    dfun <- switch(x$family$link, 
+                  probit = dnorm, 
+                  logit = dlogis,
+                  cauchit = dcauchy,
+                  log = exp,
+                  cloglog = function(z) 1 - exp(-exp(z)),
+                  inverse = function(z) 1/z,
+                  identity = function(z) 1,
+                  sqrt = function(z) z^2,
+                  "1/mu^2" = function(z) z^(-0.5),
+                  stop("Unrecognized link function")
+                  )
+    sfun <- switch(x$family$link, 
+                  probit = function(z) -z, 
+                  logit = function(z) 1 - 2 * plogis(z),
+                  cauchit = function(z) 1, # not setup
+                  log = function(z) 1, # not setup
+                  cloglog = function(z) 1, # not setup
+                  inverse = function(z) 1, # not setup
+                  identity = function(z) 1, # not setup
+                  sqrt = function(z) 1, # not setup
+                  "1/mu^2" = function(z) 1, # not setup
+                  stop("Unrecognized link function")
+                  )
     b.est <- coef(x)
-    if(!is.null(at)){
-        x$model[,names(at)] <- at
-    }
-    mm <- model.matrix(x)
-    xb <- t(as.matrix(colMeans(mm))) %*% b.est
-    
-    famlink <- paste0(x$family$family, x$family$link)
-    dfun <- switch(famlink, 
-                  binomialprobit = dnorm, 
-                  binomiallogit = dlogis,
-                  binomialcauchit = dcauchy,
-                  binomiallog = exp,
-                  binomialcloglog = function(z) 1 - exp(-exp(z)),
-                  Gammainverse = function(z) 1/z,
-                  Gammaidentity = function(z) 1,
-                  Gammalog = exp,
-                  poissonlog = exp,
-                  poissonidentity = function(z) 1,
-                  poissonsqrt = function(z) z^2,
-                  gaussianidentity = function(z) 1,
-                  gaussianlog = exp,
-                  gaussianinverse = function(z) 1/z,
-                  "inverse.gaussian1/mu^2" = NULL,
-                  inverse.gaussianinverse = function(z) -1/z,
-                  inverse.gaussianidentity = function(z) 1,
-                  inverse.gaussianlog = exp,
-                  quasilogit = dlogis,
-                  quasiprobit = dnorm,
-                  quasicloglog = NULL,
-                  quasiidentity = function(z) 1,
-                  quasiinverse = function(z) 1/z,
-                  quasilog = exp,
-                  "quasi1/mu^2" = NULL,
-                  quasisqrt = function(z) z^2,
-                  stop("Unrecognized model family or link function")
-                  )
-    sfun <- switch(famlink, 
-                  binomialprobit = function(z) -z, 
-                  binomiallogit = function(z) 1 - 2 * plogis(z),
-                  binomialcauchit = NULL,
-                  binomiallog = NULL,
-                  binomialcloglog = NULL,
-                  Gammainverse = NULL,
-                  Gammaidentity = NULL,
-                  Gammalog = NULL,
-                  poissonlog = NULL,
-                  poissonidentity = NULL,
-                  poissonsqrt = NULL,
-                  gaussianidentity = function(z) z,
-                  gaussianlog = NULL,
-                  gaussianinverse = NULL,
-                  "inverse.gaussian1/mu^2" = NULL,
-                  inverse.gaussianinverse = NULL,
-                  inverse.gaussianidentity = NULL,
-                  inverse.gaussianlog = NULL,
-                  quasilogit = NULL,
-                  quasiprobit = NULL,
-                  quasicloglog = NULL,
-                  quasiidentity = NULL,
-                  quasiinverse = NULL,
-                  quasilog = NULL,
-                  "quasi1/mu^2" = NULL,
-                  quasisqrt = NULL,
-                  stop("Unrecognized model family or link function")
-                  )
     if(atmeans) {
-        out <- data.frame(Effects = dfun(t(as.matrix(colMeans(mm))) %*% b.est) * b.est, row.names = names(b.est))
+        tmp <- as.list(colMeans(x$model))
+        if(!is.null(at)){
+            if(any(! names(at) %in% names(x$model)))
+                stop("Unrecognized variable name in 'at'")
+            tmp <- expand.grid(append(tmp, list(a = 1:2)))
+        }
+        x$model <- as.data.frame(tmp)
     } else {
-        out <- data.frame(Effects = mean(dfun(as.matrix(mm) %*% b.est)) * b.est, row.names = names(b.est))
+        if(!is.null(at)){
+            if(any(! names(at) %in% names(x$model)))
+                stop("Unrecognized variable name in 'at'")
+            x$model[,names(at)] <- at
+        }
     }
-    s <- sfun(xb)
-    dr <- (out$Effects/b.est) * (diag(1, ncol(mm), ncol(mm)) + c(s) * (b.est %*% t(as.matrix(colMeans(mm)))))
+    mm <- model.matrix(x) # automatically generates interactions & factors
+    if(any(attributes(terms(x))$order > 1))
+        warning("Interactions not currently handled correctly")
+    e <- mean(dfun(as.matrix(mm) %*% b.est)) * b.est
+    # drop non-unique I() terms
+    anyI <- grepl("I\\(.+\\)", names(e))
+    if(anyI) {
+        tmpnames <- gsub("[(I\\()(\\))]", "", names(e))
+        tmpnames <- gsub("^[:digit:]+[\\^\\+\\-\\*\\/]", "", tmpnames)
+        tmpnames <- gsub("[^\\+-\\*/][[:digit:]+]$", "", tmpnames)
+        e <- e[unique(tmpnames)]
+    }
+    
+    out <- data.frame(Factor = names(e), 
+                      Effect = e, 
+                      row.names = seq_along(e))
+    s <- sfun(t(as.matrix(colMeans(mm))) %*% b.est)
+    dr <- (out$Effect/b.est) * ( diag(1, ncol(mm), ncol(mm)) + 
+                                 c(s) * ( b.est %*% t(as.matrix(colMeans(mm))) )
+                                 )
     variance <- dr %*% vcov(x) %*% t(dr)
-    out[["Std. Error"]] <- sqrt(diag(variance))
-    out[["t.statistic"]] <- out[["Effects"]]/out[["Std. Error"]]
-    out[["Pr(>|z|)"]] <- 2 * (1 - pt(abs(out[["t.statistic"]]), nrow(mm) - ncol(mm)))
+    #out[["Std. Error"]] <- sqrt(diag(variance))
+    #out[["t.statistic"]] <- out[["Effect"]]/out[["Std. Error"]]
+    #out[["Pr(>|z|)"]] <- 2 * (1 - pt(abs(out[["t.statistic"]]), nrow(mm) - ncol(mm)))
     return(structure(out, class = c("margins", "data.frame")))
 }
 
--- R/plot.R
+++ R/plot.R
@@ -1,18 +1,30 @@
 plot.margins <- 
 function(x, 
-         which = row.names(x), 
-         include.intercept = FALSE, 
+         which = x$Factor, 
          labels = which, 
+         level = 0.95,
          bg = "black", 
          lwd = 2, 
+         intercept = FALSE,
+         vline = NULL,
          ...) {
     x <- x[which,]
-    if(!include.intercept)
-        x <- x[!row.names(x) == "(Intercept)",]
-    lb <- x[,1] - (1.96 * x[,2])
-    ub <- x[,1] + (1.96 * x[,2])
-    r <- max(lb) - min(lb)
-    dotchart(x[,1], labels = labels, bg = bg, xlim = c(min(lb)-0.04*r, max(ub)+0.04*r), ...)
-    segments(lb, 1:nrow(x), ub, 1:nrow(x), col = bg, lwd = lwd)
+    if(!intercept) {
+        x <- x[!x$Factor == "(Intercept)",]
+        labels <- labels[!labels == "(Intercept)"]
+    } 
+    quantiles <- qnorm(cbind((1-sort(level))/2, 1-(1-sort(level))/2))
+    maxl <- max(abs(quantiles))
+    lb <- x$Effect - (maxl * x[["Std. Error"]])
+    ub <- x$Effect + (maxl * x[["Std. Error"]])
+    r <- max(ub) - min(lb)
+    dotchart(x$Effect, labels = as.character(labels), bg = bg, xlim = c(min(lb)-0.04*r, max(ub)+0.04*r), ...)
+    if(!is.null(vline))
+        abline(v = vline, col = "gray")
+    mapply(function(z, lwd) {
+        segments(x$Effect + (quantiles[z,1] * x[["Std. Error"]]), 1:nrow(x), 
+                 x$Effect + (quantiles[z,2] * x[["Std. Error"]]), 1:nrow(x), 
+                 col = bg, lwd = lwd)
+    }, 1:nrow(quantiles), seq(max(lwd), 0.25, length.out = nrow(quantiles)))
     invisible(x)
 }
