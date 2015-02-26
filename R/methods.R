0b27b2eb3a3103a29beb417b0dd38887aad3f535
leeper <thosjleeper@gmail.com>
2015-02-26 21:36:32 +0100

    reduce printed digits to 4

--- R/methods.R
+++ R/methods.R
@@ -1,18 +1,21 @@
 print.margins <- 
-function(x, digits = getOption('digits',4), row.names = FALSE, ...){
-    print(summary(x), digits = digits, row.names = row.names, ...)
+function(x, digits = 4, row.names = FALSE, ...){
+    print(summary(x, digits = digits), row.names = row.names, ...)
     invisible(x)
 }
 
 summary.margins <- 
-function(object, ...){
+function(object, digits = 4, ...){
+    fmt <- paste0("%0.", ifelse(digits > 7, 7, digits), "f")
     tab <- data.frame(Factor = colnames(object$Effect), 
                       "dy/dx" = colMeans(object$Effect),
                       "Std.Err." = sqrt(object$Variance),
                       check.names = FALSE, stringsAsFactors = FALSE)
     tab[,"z value"] <- tab[,"dy/dx"]/tab[,"Std.Err."]
     tab[,"Pr(>|z|)"] <- 2 * pnorm(abs(tab[,"z value"]), lower.tail = FALSE)
-    cbind(tab, confint(object))
+    tab <- cbind(tab, confint(object))
+    for(i in 2:ncol(tab)) tab[,i] <- sprintf(fmt, tab[, i])
+    tab
 }
 
 confint.margins <- 
