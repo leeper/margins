53606a2f8ff75c371ca12b879dcf8ecda8beed90
Thomas J. Leeper <thosjleeper@gmail.com>
2016-07-30 21:23:03 +0100

    write golder quadratic tests [skip ci]

--- tests/testthat/golder.R
+++ tests/testthat/golder.R
@@ -1,6 +1,4 @@
 # formulae from Matt Golder's examples
-# http://mattgolder.com/wp-content/uploads/2015/05/standarderrors1.png
-# http://mattgolder.com/wp-content/uploads/2015/05/standarderrors2.png
 
 # example data for tests
 set.seed(1)
@@ -11,24 +9,25 @@ z <- rnorm(n)
 y <- w + x + z + w*x + w*z + x*z * w*x*z + rnorm(n)
 
 context("Golder Tests (Interaction Terms)")
-test_that("Golder Case 1a/1b correct", {
+# http://mattgolder.com/wp-content/uploads/2015/05/standarderrors1.png
+
+test_that("Golder Interaction Case 1a/1b correct", {
     f1.1 <- y ~ x + z + x:z
     m <- lm(f1.1)
     s <- summary(margins(m)
-    # with respect to x
-    ## NOTE: coefficient vector begins with intercept so use character extraction for clarity
+    # ME with respect to x
     dydx <- coef(m)["x"] + (z * coef(m)["x:z"])
     sedydx <- sqrt(vcov(m)["z","z"] + (z^2 * vcov(m)["x:z","x:z"]) + (2 * z * vcov(m)["z","x:z"]))
     expect_equal(dydx, s[s[["Factor"]] == "x", "dy/dx"], label = "dy/dx correct")
     expect_equal(sedydx, s[s[["Factor"]] == "x", "Std.Err."], label = "Var(dy/dx) correct")
-    # with respect to z
+    # ME with respect to z
     dydz <- coef(m)[1+2] + (x * coef(m)[3])
     sedydz <- sqrt(vcov(m)["z","z"] + (x^2 * vcov(m)["x:z","x:z"]) + (2 * x * vcov(m)["x","x:z"]))
     expect_equal(dydz, s[s[["Factor"]] == "z", "dy/dx"], label = "dy/dz correct")
     expect_equal(sedydz, s[s[["Factor"]] == "z", "Std.Err."], label = "Var(dy/dz) correct")
 })
 
-test_that("Golder Case 2 correct", {
+test_that("Golder Interaction Case 2 correct", {
     f1.2 <- y ~ x + z + w + x:z + z:w
     m <- lm(f1.2)
     s <- summary(margins(m)
@@ -38,7 +37,7 @@ test_that("Golder Case 2 correct", {
     expect_equal(sedydx, s[s[["Factor"]] == "x", "Std.Err."], label = "Var(dy/dx) correct")
 })
 
-test_that("Golder Case 3 correct", {
+test_that("Golder Interaction Case 3 correct", {
     f1.3 <- y ~ x + z + w + x:z + x:w + z:w
     m <- lm(f1.3)
     s <- summary(margins(m)
@@ -49,7 +48,7 @@ test_that("Golder Case 3 correct", {
     expect_equal(sedydx, s[s[["Factor"]] == "x", "Std.Err."], label = "Var(dy/dx) correct")
 })
 
-test_that("Golder Case 4 correct", {
+test_that("Golder Interaction Case 4 correct", {
     f1.4 <- y ~ x + z + w + x:z + x:w + z:w + x:z:w
     m <- lm(f1.4)
     s <- summary(margins(m)
@@ -63,12 +62,66 @@ test_that("Golder Case 4 correct", {
     expect_equal(sedydx, s[s[["Factor"]] == "x", "Std.Err."], label = "Var(dy/dx) correct")
 })
 
+context("Golder Tests (Quadratic Terms)")
+# http://mattgolder.com/wp-content/uploads/2015/05/standarderrors2.png
 
+test_that("Golder Quadratic Case 1 correct", {
+    f2.1 <- y ~ x + I(x^2)
+    m <- lm(f2.1)
+    s <- summary(margins(m)
+    dydx <- coef(m)["x"] + (2 * coef(m)["I(x^2)"] * x)
+    sedydx <- sqrt(vcov(m)["x","x"] + (4 * x^2 * vcov(m)["I(x^2)","I(x^2)"]) + (4 * x * vcov(m)["x","I(x^2)"]))
+    expect_equal(dydx, s[s[["Factor"]] == "x", "dy/dx"], label = "dy/dx correct")
+    expect_equal(sedydx, s[s[["Factor"]] == "x", "Std.Err."], label = "Var(dy/dx) correct")
+})
 
-f2.1 <- y ~ x + I(x^2)
-f2.2 <- y ~ x + I(x^2) + z
-f2.3a <- y ~ x + I(x^2) + z + x:z # with respect to x
-f2.3b <- y ~ x + I(x^2) + z + x:z # with respect to z
-f2.4a <- y ~ x + I(x^2) + z + x:z + I(x^2):z # with respect to x
-f2.4b <- y ~ x + I(x^2) + z + x:z + I(x^2):z # with respect to z
+test_that("Golder Quadratic Case 2 correct", {
+    f2.2 <- y ~ x + I(x^2) + z
+    m <- lm(f2.2)
+    s <- summary(margins(m)
+    dydx <- coef(m)["x"] + (2 * coef(m)["I(x^2)"] * x)
+    sedydx <- sqrt(vcov(m)["x","x"] + (4 * x^2 * vcov(m)["I(x^2)","I(x^2)"]) + (4 * x * vcov(m)["x","I(x^2)"]))
+    expect_equal(dydx, s[s[["Factor"]] == "x", "dy/dx"], label = "dy/dx correct")
+    expect_equal(sedydx, s[s[["Factor"]] == "x", "Std.Err."], label = "Var(dy/dx) correct")
+})
+
+test_that("Golder Quadratic Case 3a/3b correct", {
+    f2.3 <- y ~ x + I(x^2) + z + x:z
+    m <- lm(f2.3)
+    s <- summary(margins(m)
+    # ME with respect to x
+    dydx <- coef(m)["x"] + (2 * coef(m)["I(x^2)"] * x) + (z * coef(m)["x:z"])
+    sedydx <- sqrt(vcov(m)["x","x"] + (4 * x^2 * vcov(m)["I(x^2)","I(x^2)"]) + (z^2 * vcov(m)["x:z","x:z"]) +
+                   (4 * x * vcov(m)["x","I(x^2)"]) + (2 * z * vcov(m)["x","x:z"]) + (4 * x * z * vcov(m)["I(x^2)", "x:z"]) )
+    expect_equal(dydx, s[s[["Factor"]] == "x", "dy/dx"], label = "dy/dx correct")
+    expect_equal(sedydx, s[s[["Factor"]] == "x", "Std.Err."], label = "Var(dy/dx) correct")
+    # ME with respect to z
+    dydz <- coef(m)["z"] + (x * coef(m)["x:z"])
+    sedydz <- sqrt(vcov(m)["z","z"] + (x^2 * vcov(m)["x:z","x:z"]) + (2 * x * vcov(m)["z","x:z"]))
+    expect_equal(dydz, s[s[["Factor"]] == "x", "dy/dx"], label = "dy/dz correct")
+    expect_equal(sedydz, s[s[["Factor"]] == "x", "Std.Err."], label = "Var(dy/dz) correct")
+})
+
+test_that("Golder Quadratic Case 4a/4b correct", {
+    f2.4a <- y ~ x + I(x^2) + z + x:z + I(x^2):z
+    m <- lm(f2.4)
+    s <- summary(margins(m)
+    # ME with respect to x
+    dydx <- coef(m)["x"] + (2 * coef(m)["I(x^2)"] * x) + (z * coef(m)["x:z"]) + (2 * x * z * coef(m)["I(x^2):z"])
+    sedydx <- sqrt(vcov(m)["x","x"] + (4 * x^2 * vcov(m)["I(x^2)","I(x^2)"]) + (z^2 * vcov(m)["x:z","x:z"]) +
+                   (4 * (x^2) * (z^2) * vcov(m)["x","I(x^2)"]) + (4 * x * vcov(m)["x","I(x^2)"]) + 
+                   (4 * z * vcov(m)["x","x:z"]) + (4 * x * z * vcov(m)["I(x^2)", "x:z"]) +
+                   (4 * x * z * vcov(m)["x","I(x^2):z"]) + (8 * (x^2) * z * vcov(m)["I(x^2)","I(x^2):z"]) + 
+                   (4 * x * (z^2) * vcov(m)["x:z","I(x^2):z"]) )
+    expect_equal(dydx, s[s[["Factor"]] == "x", "dy/dx"], label = "dy/dx correct")
+    expect_equal(sedydx, s[s[["Factor"]] == "x", "Std.Err."], label = "Var(dy/dx) correct")
+    # ME with respect to z
+    dydz <- coef(m)["z"] + (x * coef(m)["x:z"]) + (x^2 * coef(m)["I(x^2):z"])
+    sedydz <- sqrt(vcov(m)["z","z"] + (x^2 * vcov(m)["x:z","x:z"]) + (x^4 * vcov(m)["I(x^2):z","I(x^2):z"]) + 
+                   (2 * x * vcov(m)["z","x:z"]) + (2 * (x^2) * vcov(m)["z","I(x^2):z"]) + 
+                   (2 * (x^3) * vcov(m)["x:z","I(x^2):z"]) )
+    expect_equal(dydz, s[s[["Factor"]] == "x", "dy/dx"], label = "dy/dz correct")
+    expect_equal(sedydz, s[s[["Factor"]] == "x", "Std.Err."], label = "Var(dy/dz) correct")
+})
+
 
