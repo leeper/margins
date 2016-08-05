3e171c27f94b2403dbc10ece961eda5c569fa37b
Thomas J. Leeper <thosjleeper@gmail.com>
2016-08-05 13:44:49 +0100

    update docs (#16)

--- R/build_margins.R
+++ R/build_margins.R
@@ -7,14 +7,11 @@
 #' @param iterations If \code{vce = "bootstrap"}, the number of bootstrap iterations. If \code{vce = "simulation"}, the number of simulated effects to draw. Ignored otherwise.
 #' @param method A character string indicating the numeric derivative method to use when estimating marginal effects. \dQuote{simple} optimizes for speed; \dQuote{Richardson} optimizes for accuracy. See \code{\link[numDeriv]{grad}} for details.
 #' @param \dots Ignored.
-#' @details Generally, it is not necessary to call this function directly because \code{\link{margins}} provides a simpler interface. To just get marginal effects without building a \dQuote{margins} object, call \code{\link{marginal_effects}}, which handles the actual differentiation of a model object.
+#' @details Generally, it is not necessary to call this function directly because \code{\link{margins}} provides a simpler interface. To just get marginal effects without building a \dQuote{margins} object, call \code{\link{marginal_effects}} instead, which handles the effect estimation of a model object without building a \dQuote{margins} object.
 #' 
-#' This is the internal, workhorse function that estimates marginal effects from model \code{model} for a data.frame, \code{data}, optionally estimating either \dQuote{average marginal effects} (when \code{atmeans = FALSE}) or \dQuote{marginal effects at means} (when \code{atmeans = TRUE}). In the former case, the marginal effects are estimated for each observation in the dataset and returned in full. In the latter, column means are taken for \code{data} and estimation is performed only these \dQuote{averaged} cases. The former is generally preferred because the latter may estimate marginal effects for cases that are unintuitive or not covered by the observed data (e.g., the effect when a binary variable in \code{data} is averaged to 0.6 rather than at 0 and 1, respectively).
-#' 
-#' To estimate marginal effects at specified values of \code{model} (other than means), use \code{\link{margins}} with the \code{at} parameter to specify values of covariates at which to estimate effects.
+#' This is the package's core function that assembles a \dQuote{margins} object, through sequential calls to \code{\link{prediction}}, \code{\link{marginal_effects}}, and an internal function (\code{get_effect_variances()}) to calculate variances. See documentation pages for those functions for details on implementation and return values.
 #' 
 #' The choice of \code{vce} may be important. The default variance-covariance estimation procedure (\code{vce = "delta"}) uses the delta method to estimate marginal effect variances. This is the fastest method. When \code{vce = "simulation"}, coefficient estimates are repeatedly drawn from the asymptotic (multivariate normal) distribution of the model coefficients and each draw is used to estimate marginal effects, with the variance based upon the dispersion of those simulated effects. The number of interations used is given by \code{iterations}. For \code{vce = "bootstrap"}, the bootstrap is used to repeatedly subsample \code{data} and the variance of marginal effects is estimated from the variance of the bootstrap distribution. This method is markedly slower than the other two procedures and, obviously, it will probably fail if \code{atmeans = TRUE}. Again, \code{iterations} regulates the number of boostrap subsamples to draw.
-#' 
 #'
 #' @return A data.frame of class \dQuote{margins} containing the contents of \code{data}, fitted values for \code{model}, the standard errors of the fitted values, and any estimated marginal effects. This data.frame may have repeated column names (for the original variables and the margginal effects thereof). Marginal effects columns are distinguished by their class (\dQuote{marginaleffect}) and can be extracted using \code{\link{extract_marginal_effects}}. Attributes containing additional information, including the marginal effect variances and additional details.
 #' @seealso \code{\link{margins}}, \code{\link{marginal_effects}}
@@ -43,17 +40,16 @@ function(model,
     
     # obtain gradient with respect to each variable in data
     mes <- marginal_effects(model = model, data = data, type = type, method = method)
-    mes_out <- mes[, grep(allvars, names(mes)), drop = FALSE]
     
     # variance estimation technique
-    variances <- get_effect_variances(data = data, model = model, allvars = allvars, 
+    variances <- get_effect_variances(data = data, model = model, allvars = names(mes), 
                                       type = type, vce = vce, iterations = iterations, method = method)
     
     # obtain predicted values and standard errors
     pred <- prediction(model = model, data = data, type = type)
     
     # setup output structure
-    structure(cbind(data, pred, mes_out), 
+    structure(cbind(data, pred, mes), 
               class = c("margins", "data.frame"), 
               Variances = setNames(variances, names(mes)),
               type = type,
--- R/margins.R
+++ R/margins.R
@@ -6,16 +6,22 @@
 #' @param at A list of one or more named vectors, specifically values at which to calculate the marginal effects. See \code{\link{build_datalist}} for details on use.
 #' @param atmeans A logical indicating whether to calculate marginal effects at the means (i.e., partial effects at the average of all covariates), as opposed to the default average marginal effects (i.e., average partial effects), which is the default.
 #' @param \dots Arguments passed to methods, and in turn to \code{\link{build_margins}}.
-#' @details Calculates marginal effects for a linear model object using \code{\link{marginal_effects}} and returns a \dQuote{marginslist} list of one or more \dQuote{margins} objects.
+#' @details Methods for this generic return a \dQuote{marginslist} list of one or more \dQuote{margins} objects. A \dQuote{margins} object is a data.frame consisting of the original data, predicted values and standard errors thereof, estimated marginal effects from the model \code{model}, with attributes describing various features of the marginal effects estimates.
+#' 
+#' See \code{\link{marginal_effects}} for details on estimation of marginal effects.
+#'
+#' The \code{atmeans} option controls what types of effects are returned. Estimates are either \dQuote{average marginal effects} (when \code{atmeans = FALSE}, the default) or \dQuote{marginal effects at means} (when \code{atmeans = TRUE}). In the former case, the marginal effects are estimated for each observation in the dataset and returned in full. In the latter, column means are taken for \code{data} and estimation is performed only these \dQuote{averaged} cases. The former is generally preferred because the latter may estimate marginal effects for cases that are unintuitive or not covered by the observed data (e.g., the effect when a binary variable in \code{data} is averaged to 0.6 rather than at 0 and 1, respectively).
+#' 
+#' The \code{margins} method for objects of class \dQuote{lm} or \dQuote{glm} simply constructs a list of data.frames (using \code{\link{build_datalist}}) and calls \code{\link{build_margins}} on each. You can call \code{build_margins} directly, but it requires the explicit specification of a dataset over which to estimate the quantities of interest.
+#' 
+#' Alternatively, you can use \code{\link{marginal_effects}} to retrieve a data.frame of marginal effects without constructing a \dQuote{margins} object. That can be efficient for plotting, etc., given the time-consuming nature of variance estimation. Use \code{\link{extract_marginal_effects}} to retrieve a data.frame of marginal effects from a \dQuote{margins} object.
+#' 
 #' @return An object of class \dQuote{marginslist}, composed of one or more objects of class \dQuote{margins}.
 #' @author Thomas J. Leeper
 #' @references
 #' Greene, W.H. 2012. Econometric Analysis, 7th Ed. Boston: Pearson.
 #' 
 #' Stata manual: \code{margins}. Retrieved 2014-12-15 from \url{http://www.stata.com/manuals13/rmargins.pdf}.
-#' @details Alternatively, use \code{\link{marginal_effects}} to retrieve only marginal effects without constructing a \dQuote{margins} object.
-#' 
-#' Use \code{\link{extract_marginal_effects}} to retrieve a data.frame of marginal effects from a \dQuote{margins} object.
 #' @examples
 #' # linear model
 #' require("datasets")
