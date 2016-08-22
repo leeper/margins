280b3b5463c7763262ede28a9abb9b4d89daa328
Thomas J. Leeper <thosjleeper@gmail.com>
2016-08-22 16:03:32 +0100

    add a ... argument prediction()

--- R/prediction.R
+++ R/prediction.R
@@ -3,6 +3,7 @@
 #' @param model A model object, perhaps returned by \code{\link[stats]{lm}} or \code{\link[stats]{glm}}.
 #' @param data A data.frame over which to calculate marginal effects.
 #' @param type A character string indicating the type of marginal effects to estimate. Mostly relevant for non-linear models, where the reasonable options are \dQuote{response} (the default) or \dQuote{link} (i.e., on the scale of the linear predictor in a GLM).
+#' @param \dots Additional arguments passed to \code{\link[stats]{predict}} methods.
 #' @details This function is simply a wrapper around \code{\link[stats]{predict}} that returns a data.frame containing predicted values with respect to all variables specified in \code{data}. It is used internally by \code{\link{build_margins}}.
 #' @return An data.frame with number of rows equal to number of rows in \code{data}, where each row is an observation and the two columns represent fitted/predicted values and the standard errors thereof.
 #' require("datasets")
@@ -12,7 +13,7 @@
 #' @seealso \code{\link{marginal_effects}}, \code{\link{build_margins}}
 #' @keywords models
 #' @export
-prediction <- function(model, data, type = c("response", "link")) {
+prediction <- function(model, data, type = c("response", "link"), ...) {
     # setup data
     if (missing(data)) {
         if (!is.null(model[["call"]][["data"]])) {
@@ -25,7 +26,7 @@ prediction <- function(model, data, type = c("response", "link")) {
     type <- match.arg(type)
     
     # extract predicted value at input value (value can only be 1 number)
-    pred <- predict(model, newdata = data, type = type, se.fit = TRUE)
+    pred <- predict(model, newdata = data, type = type, se.fit = TRUE, ...)
     class(pred[["fit"]]) <- c("fit", "numeric")
     class(pred[["se.fit"]]) <- c("se.fit", "numeric")
     
