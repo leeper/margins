2249b278de4c72bbe9eae2ee6e70d20cc77723b6
Thomas J. Leeper <thosjleeper@gmail.com>
2018-01-26 17:13:01 +0000

    rm unnecessary warn_for_weights() func

--- R/warn_for_weights.R
+++
@@ -1,7 +0,0 @@
-warn_for_weights <- function(model) {
-    wghts <- unname(model[["weights"]])
-    if (!isTRUE(all.equal(wghts, rep(wghts[1], length(wghts))))) {
-        warning("'weights' used in model estimation are currently ignored!", call. = FALSE)
-    }
-    NULL
-}
