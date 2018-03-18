2db427616dedfd3d764faa6158b4c0b8a0991b78
Thomas J. Leeper <thosjleeper@gmail.com>
2018-03-18 14:23:19 +0000

    add support for betareg (closes #90)

--- R/find_terms_in_model.R
+++ R/find_terms_in_model.R
@@ -4,9 +4,48 @@
 ## - `lnames` (logical variables)
 ## - `fnames` (factor variables)
 find_terms_in_model <- function(model, variables = NULL) {
+    UseMethod("find_terms_in_model")
+}
+
+find_terms_in_model.default <- function(model, variables = NULL) {
     
     # identify classes of terms in `model`
     classes <- attributes(terms(model))[["dataClasses"]][-1]
+    # drop specially named "(weights)" variables
+    wts <- weights(model)
+    if (!is.null(wts)) {
+        classes <- classes[!names(classes) %in% "(weights)"]
+    }
+    # handle character variables as factors
+    classes[classes == "character"] <- "factor"
+    ## cleanup names of terms
+    names(classes) <- clean_terms(names(classes))
+    
+    # identify factors versus numeric terms in `model`, and examine only unique terms
+    vars <- list(
+      nnames = unique(names(classes)[!classes %in% c("factor", "ordered", "logical")]),
+      lnames = unique(names(classes)[classes == "logical"]),
+      fnames = unique(names(classes)[classes %in% c("factor", "ordered")])
+    )
+    
+    # subset of variables for which to compute the marginal effects
+    if (!is.null(variables)) {
+        tmp <- c(vars$nnames, vars$lnames, vars$fnames)
+        if (any(!variables %in% tmp)) {
+            stop("Some values in 'variables' are not in the model terms.")
+        }
+        vars$nnames <- vars$nnames[vars$nnames %in% variables]
+        vars$lnames <- vars$lnames[vars$lnames %in% variables]
+        vars$fnames <- vars$fnames[vars$fnames %in% variables]
+    }
+    
+    return(vars)
+}
+
+find_terms_in_model.betareg <- function(model, variables = NULL) {
+    
+    # identify classes of terms in `model`
+    classes <- attributes(terms(model$model))[["dataClasses"]][-1]
     # drop specially named "(weights)" variables
     wts <- weights(model)
     if (!is.null(wts)) {
--- tests/testthat/tests-methods.R
+++ tests/testthat/tests-methods.R
@@ -3,20 +3,15 @@ tol <- 0.0001
 
 library("datasets")
 
-context("Test 'nnet' methods")
-if (requireNamespace("nnet")) {
-    data("iris3", package = "datasets")
-    ird <- data.frame(rbind(iris3[,,1], iris3[,,2], iris3[,,3]),
-                      species = factor(c(rep("s",50), rep("c", 50), rep("v", 50))))
-    m <- nnet::nnet(species ~ ., data = ird, size = 2, rang = 0.1,
-                    decay = 5e-4, maxit = 200, trace = FALSE)
-    test_that("Test marginal_effects() for 'nnet'", {
+context("Test 'betareg' methods")
+if (requireNamespace("betareg")) {
+    data("GasolineYield", package = "betareg")
+    m <- betareg(yield ~ batch + temp, data = GasolineYield)
+    test_that("Test marginal_effects() for 'betareg'", {
         expect_true(inherits(marginal_effects(m), "data.frame"))
-        expect_true(inherits(marginal_effects(m, category = "c"), "data.frame"))
     })
-    test_that("Test margins() for 'nnet'", {
+    test_that("Test margins() for 'betareg'", {
         expect_true(inherits(margins(m), "margins"))
-        expect_true(inherits(margins(m, category = "c"), "margins"))
     })
 }
 
@@ -31,6 +26,23 @@ if (requireNamespace("MASS")) {
     test_that("Test margins() for 'polr'", {
         expect_true(inherits(margins(m), "margins"))
         expect_true(inherits(margins(m, category = "Low"), "margins"))
+    })
+}
+
+context("Test 'nnet' methods")
+if (requireNamespace("nnet")) {
+    data("iris3", package = "datasets")
+    ird <- data.frame(rbind(iris3[,,1], iris3[,,2], iris3[,,3]),
+                      species = factor(c(rep("s",50), rep("c", 50), rep("v", 50))))
+    m <- nnet::nnet(species ~ ., data = ird, size = 2, rang = 0.1,
+                    decay = 5e-4, maxit = 200, trace = FALSE)
+    test_that("Test marginal_effects() for 'nnet'", {
+        expect_true(inherits(marginal_effects(m), "data.frame"))
+        expect_true(inherits(marginal_effects(m, category = "c"), "data.frame"))
+    })
+    test_that("Test margins() for 'nnet'", {
+        expect_true(inherits(margins(m), "margins"))
+        expect_true(inherits(margins(m, category = "c"), "margins"))
     })
 }
 
